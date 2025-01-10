fc75bb68aa6accdf7f19c862db4facb7f49c2aec
blevon24 <brianna.levon24@gmail.com>
2025-01-10 04:05:56 -0500

    Delete Catacomb Game (CS Project #4)/Monster.java

--- Catacomb Game (CS Project #4)/Monster.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Monster extends Actor{
-  
-  public Monster(String name, int health, int maxDamage, int row, int col){
-    super(name, health, maxDamage, row, col);
-  }
-  
-}
